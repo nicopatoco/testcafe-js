16b60aa0c38043a4f87d5d6e224ef340ea163e02
Nicolas Herrera <NP.HERRERA@GLOBANT.COM>
2020-10-04 16:40:21 -0300

    Assertions for the white titles

--- test/testCafeTest.js
+++ test/testCafeTest.js
@@ -4,5 +4,11 @@ fixture('getting started').page("https://devexpress.github.io/testcafe/");
 
 test('first test', async t => {
     await t
-        .maximizeWindow();
+        .maximizeWindow()
+        .expect(home.firstCard.innerText).eql('Create stable tests\n(and no manual timeouts)', { timeout: 500 })
+        .expect(home.secondCard.innerText).eql('Write in latest JS\nor TypeScript', { timeout: 500 })
+        .expect(home.thirdCard.innerText).contains('Detect JS errors', { timeout: 500 })
+        .expect(home.fourthCard.innerText).contains('Launch', { timeout: 500 })
+        .expect(home.fivethCard.innerText).contains('Build readable tests', { timeout: 500 })
+        .expect(home.sixthfiCard.innerText).contains('Include tests in continuous', { timeout: 500 });
 })
