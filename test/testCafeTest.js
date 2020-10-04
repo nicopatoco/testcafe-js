b600e56fc56f47a8b2226fa347a9b84c95cf9382
Nicolas Herrera <NP.HERRERA@GLOBANT.COM>
2020-10-04 17:00:41 -0300

    Update assertion and adding click to navigate to getStarted page

--- test/testCafeTest.js
+++ test/testCafeTest.js
@@ -9,6 +9,9 @@ test('first test', async t => {
         .expect(home.secondCard.innerText).eql('Write in latest JS\nor TypeScript', { timeout: 500 })
         .expect(home.thirdCard.innerText).contains('Detect JS errors', { timeout: 500 })
         .expect(home.fourthCard.innerText).contains('Launch', { timeout: 500 })
-        .expect(home.fivethCard.innerText).contains('Build readable tests', { timeout: 500 })
+        .expect(home.fivethCardContainer.find('.feature.feature-5 > p').innerText).contains('Build readable tests', { timeout: 500 })
+        .expect(home.fivethCardContainer.find('.feature-5-code').innerText).contains("const el = Selector('.column').find('label')", { timeout: 500 })
         .expect(home.sixthfiCard.innerText).contains('Include tests in continuous', { timeout: 500 });
+        
+    home.clickOnGetStartedButton();
 })
