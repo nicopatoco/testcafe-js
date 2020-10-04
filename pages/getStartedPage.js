8633b40f5e0948e1135d6ac8a8b54717309d46cd
Nicolas Herrera <NP.HERRERA@GLOBANT.COM>
2020-10-04 18:41:50 -0300

    Add title selector

--- pages/getStartedPage.js
+++ pages/getStartedPage.js
@@ -4,6 +4,7 @@ class GetSartedPage {
     constructor() {
         this.navBar = Selector(".doc-top-nav-tab")
         this.activeTab = Selector(".doc-top-nav-tab.active")
+        this.activeTabTitle = Selector(".post-content > h1")
     }
 }
 
